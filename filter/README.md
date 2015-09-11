485f80f3e4d09f1f1c1ac6656aa0efeb70b5aad5
Bhargav Teja <cervere>
2015-09-12 05:05:17 +0530

    handling image file types

--- filter/README.md
+++ filter/README.md
@@ -1,4 +1,4 @@
 # Identify duplicate images  
 Usage:
 
-python filter.py --dataset dir_of_images --shelve shelve_db
+python filter.py --dataset dir_of_images --shelve shelve_db [--ftype gif_jpg_jpeg_etc]
--- filter/filter.py
+++ filter/filter.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 import imagehash
 import argparse
 import shelve
@@ -7,19 +8,48 @@ import glob
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required = True,help = "path to input dataset of images")
 ap.add_argument("-s", "--shelve", required = True,help = "output shelve database")
+ap.add_argument("-f", "--ftype", required = False,help = "index images of type")
 args = vars(ap.parse_args())
 
-db = shelve.open(args["shelve"], writeback = True)
+filetypes = ["jpg", "jpeg", "png", "gif"]
+mask = [1,1,1,1]
 
-for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
-	filename = imagePath[imagePath.rfind("/") + 1:]
+UNIQUE = "./Unique"
+DUPLICATE = "./Duplicates"
+
+def index(folder, type="jpg", reindex=False):
+    for imagePath in glob.glob(folder + "/*."+type):
+        filename = imagePath[imagePath.rfind("/") + 1:]
 	image = Image.open(imagePath)
 	h = str(imagehash.dhash(image))
         if db.has_key(h):
-            print "Already there"
-            image.save("../Duplicates_JPG/"+filename)
-        else:
-            image.save("../Unique_JPG/"+filename)
+            print "Already there, copying to duplicates"
+            image.save(DUPLICATE+"/"+filename)
+        elif not reindex:
+            image.save(UNIQUE+"/"+filename)
 	db[h] = db.get(h, []) + [filename]
+
+def indexall(folder, reindex=False):
+    for ftype in filetypes:
+        index(folder, ftype, reindex)
+
+if not os.path.exists(UNIQUE):
+    os.makedirs(UNIQUE)
+if not os.path.exists(DUPLICATE):
+    os.makedirs(DUPLICATE)
+
+db = shelve.open(UNIQUE+"/"+args["shelve"], writeback = True)
+
+if not os.path.exists(UNIQUE+"/"+args["shelve"]):
+    print "no shelve found in %s" % (UNIQUE+"/"+args["shelve"])
+    print "re-indexing the folder"
+    indexall(UNIQUE, reindex=True)
+    
+folder = args["dataset"]
+if args.has_key("ftype"):
+    ftype = args["ftype"]
+    index(folder, ftype)
+else:
+    indexall(folder)
 
 db.close()
