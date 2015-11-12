711b325e687d116d33046f3a77655406a397581b
partha <pprotim80@gmail.com>
2015-11-13 00:48:00 +0530

    Removing unwanted comments.

--- admintool/adv/WebContent/js/crawl.js
+++ admintool/adv/WebContent/js/crawl.js
@@ -335,34 +335,7 @@ app.controller('CrawlController',['$scope', '$rootScope','$http', '$interval', '
 
 angular.module('adminApp').controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 
-		 /* $scope.items = ['item1', 'item2', 'item3'];
-
-		  $scope.animationsEnabled = true;
-
-		  $scope.open = function (size) {
-
-		    var modalInstance = $modal.open({
-		      animation: $scope.animationsEnabled,
-		      templateUrl: 'myModalContent.html',
-		      controller: 'ModalInstanceCtrl',
-		      size: size,
-		      resolve: {
-		        items: function () {
-		          return $scope.items;
-		        }
-		      }
-		    });
-
-		    modalInstance.result.then(function (selectedItem) {
-		      $scope.selected = selectedItem;
-		    }, function () {
-		      $log.info('Modal dismissed at: ' + new Date());
-		    });
-		  };
-
-		  $scope.toggleAnimation = function () {
-		    $scope.animationsEnabled = !$scope.animationsEnabled;
-		  };*/
+		
 
 });
 
